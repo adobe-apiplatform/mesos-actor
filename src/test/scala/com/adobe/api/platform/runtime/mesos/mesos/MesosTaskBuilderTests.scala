2105639a70a63b5ec90644104dca3586514340e4
Himavanth <contacthima@yahoo.com>
2018-10-02 14:33:40 +0530

    [RUNNNER-957] Build fix

--- src/test/scala/com/adobe/api/platform/runtime/mesos/mesos/MesosTaskBuilderTests.scala
+++ src/test/scala/com/adobe/api/platform/runtime/mesos/mesos/MesosTaskBuilderTests.scala
@@ -64,8 +64,7 @@ class MesosTaskBuilderTests extends FlatSpec with Matchers {
       true,
       User("usernet"),
       parameters,
-      Some(CommandDef(environment = environment)),
-      healthCheckParams = Map("timeout" -> 2, "maxConsecutiveFailures" -> 2))
+      Some(CommandDef(environment = environment)))
     val taskInfo = new DefaultTaskBuilder()(taskDef, offers.getOffers(0), resources, portMappings)
 
     taskInfo.getTaskId.getValue shouldBe taskDef.taskId
